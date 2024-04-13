6418ded815e97d19f293c888543115677ea100c4
Praphul Kumar <praphulsingh1712@gmail.com>
2024-04-14 02:38:29 +0530

    defining the signup routes

--- src/app/api/signup/signup.ts
+++ src/app/api/signup/signup.ts
@@ -8,6 +8,82 @@ export async function POST(request:Request){
 
    try {
     const {email, username, password}=await request.json()
+    const existingUserByUsername=await UserModel.findOne({
+        username,
+        isVerified:true
+    })
+    if(existingUserByUsername){
+        return Response.json({
+            success:false,
+            message:"User already exists"
+        },{
+            status:400
+        })
+    }
+
+    const existingUserByEmail=await UserModel.findOne({email})
+
+    const verifyCode=Math.floor(100000+Math.random()*900000).toString()
+
+    if(existingUserByEmail){
+        if (existingUserByEmail.isVerified) {
+            return Response.json({
+            success:false,
+            message:"email already exists"
+        },{
+            status:400
+        })
+        }else{
+            const hashedPassword=await bcryptjs.hash(password, 10)
+            existingUserByEmail.password=hashedPassword
+            existingUserByEmail.verifyCode=verifyCode;
+            existingUserByEmail.verifyCodeExpiry=new Date(Date.now()+3600000)
+            await existingUserByEmail.save()
+        }
+        
+    }
+    else{
+        const hashedPassword=await bcryptjs.hash(password,10)
+        /*in below code we are setting expiry time to 1 hour and because of new keyword we can use the variable */
+        const expiryDate=new Date()
+        expiryDate.setHours(expiryDate.getHours()+1)
+
+        const newUser=new UserModel({
+            username,
+            email,
+            password: hashedPassword,
+            verifyCode: verifyCode,
+            verifyCodeExpiry: expiryDate,
+            isVerified: false,
+            isAcceptingMessage:true,
+            messages: []
+        })
+        await newUser.save()
+    }
+        /*sending email to user*/
+        const emailResponse=await sendVerificationEmail(
+            email,
+            verifyCode,
+            username
+        )
+        if (!emailResponse.success) {
+            return Response.json({
+            success:false,
+            message:"sorry! email could not be sent"
+        },{status:500})
+        }
+
+        return Response.json({
+            success:true,
+            message:"Email has been sent successfully"
+        },{status:200})
+
+
+
+
+        
+    
+
    } catch (error) {
     console.error("Error while regestering user",error)
     return Response.json({
